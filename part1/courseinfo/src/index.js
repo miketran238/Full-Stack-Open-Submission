4af85c175be01fafb044cdcf002a7d4e484ca85c
Mike Tran <ngocmike238@gmail.com>
2020-07-28 16:36:00 -0400

    part 1.4-1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
 const Part =(props) => {
+  console.log(props)
   return (
     <p>
-      {props.name} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
@@ -18,28 +19,42 @@ const Part =(props) => {
 const Content = (props) => {
   return (
     <div>
-    <Part name={props.parts[0]} exercise={props.exercises[0]} />
-    <Part name={props.parts[1]} exercise={props.exercises[1]} />
-    <Part name={props.parts[2]} exercise={props.exercises[2]} />
+    <Part part={props.course.parts[0]}  />
+    <Part part={props.course.parts[1]}  />
+    <Part part={props.course.parts[2]}  />
     </div>
   )
 }
 const Total = (props) => {
   return (
     <div>
-    <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+    <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </div>
   )
 }
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-  const exercises = [10, 7, 14]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total exercises={exercises} />
+      <Content course={course} />
+      <Total course={course} />
 
     </>
   )
