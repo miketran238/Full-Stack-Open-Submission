896f52e9c8ade08e7961f128357632bb2fe43d61
Mike Tran <ngocmike238@gmail.com>
2020-07-31 23:22:00 -0400

    Phone book 2.6-2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ nameFilter, setNewFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const personsToShow = nameFilter === ''
+    ? persons
+    : persons.filter(name => nameFilter in name)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      Filter by name 
+      <input value={nameFilter} onChange={handleNewFilter} />
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person,i) => 
+        <Person key={i} person={person} />
+        )}
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const Person = ({person}) => {
+    return (
+        <p>{person.name} {person.number}</p>
+    )
+}
+export default Person
